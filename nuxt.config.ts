46d3608e497ea2bd064c4924a27e502d4cb3a0b5
mo36924 <mo36924@users.noreply.github.com>
2023-05-20 20:41:06 +0900

    chore: enable typed pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/devtools']
+  modules: ['@nuxt/devtools'],
+  experimental: {
+    typedPages: true
+  }
 })
