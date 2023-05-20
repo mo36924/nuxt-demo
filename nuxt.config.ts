9a8d1fedffcf18ed5d1a1563d42af5b0178351b4
mo36924 <mo36924@users.noreply.github.com>
2023-05-20 20:42:24 +0900

    chore: change module resolution

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/devtools'],
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        moduleResolution: 'bundler'
+      }
+    }
+  },
   experimental: {
     typedPages: true
   }
